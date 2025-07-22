879e7ca2dbf71ff9a305c1e329075681cd313e2d
GiuseppeDeno <giuseppedeno@gmail.com>
2025-07-22 18:24:45 +0200

    classPost.php

--- Post/classPost.php
+++ Post/classPost.php
@@ -9,25 +9,7 @@ abstract class Category {
     abstract public function getMyCategory();
 }
 
-/* class News extends Category{
 
-   public $nameCategory;
-  
-   public function __construct($nameCategory){
-    $this->nameCategory=$nameCategory;
-   }
-
-    public function getMyCategory(){
-        return $this->nameCategory;
-       
-        
-
-    }
-
-//metto il costruct nel abstract cosi non devo ripeterlo per i figli
-//e nei figli implemento solo il getMyCategory
-
-} */
 
 class News extends Category {
     public function getMyCategory() {
